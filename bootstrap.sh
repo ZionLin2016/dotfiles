096a9ae5d0305a16dcc2e6085edfc43b7d64e09f
kevinhwang91 <kevin.hwang@live.com>
2019-03-15 00:36:07 +0800

    add git shell inside bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -20,6 +20,25 @@ fail() {
     exit
 }
 
+setup_git() {
+    local git_name=
+    local git_email=
+
+    if [[ ! -f $HOME/.gitconfig ]]; then
+        info "setup gitconfig"
+
+        user "What is your git user email?"
+        read -e git_email
+        user "What is you git user name"
+        read -e git_name
+
+        sed -e "s/USER_NAME/$git_name/g" -e "s/USER_EMAIL/$git_email/g" $DOTFILES_ROOT/git/gitconfig > $HOME/.gitconfig
+        
+        success "setup gitconfig"
+    fi
+}
+
+
 link_file() {
     local src=$1 dst=$2
 
@@ -74,23 +93,31 @@ link_file() {
     fi
 }
 
+setup_git
+
+# neovim
 NVIM_HOME=$HOME/.config/nvim
 D_NVIM_HOME=$DOTFILES_ROOT/nvim
 mkdir -p $NVIM_HOME/autoload 2>/dev/null
 
-curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
-https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+if [[ ! -f $NVIM_HOME/autoload/plug.vim ]]; then
+    curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
 
 link_file $D_NVIM_HOME/init.vim $NVIM_HOME/init.vim
 link_file $D_NVIM_HOME/rm_deleted_undo.sh $NVIM_HOME/rm_deleted_undo
-link_file $D_NVIM_HOME/coc-settings.json $NVIM_HOME/coc-settings.json
 
+# neovim language-server
+link_file $D_NVIM_HOME/coc-settings.json $NVIM_HOME/coc-settings.json
 mkdir -p $HOME/.config/coc/extensions 2>/dev/null
 link_file $D_NVIM_HOME/coc-package.json $HOME/.config/coc/extensions/package.json
 
+# idea
 D_IDEA_HOME=$DOTFILES_ROOT/idea
 link_file $D_IDEA_HOME/ideavimrc $HOME/.ideavimrc
 
+# tmux
 D_TMUX_HOME=$DOTFILES_ROOT/tmux
 link_file $D_TMUX_HOME/tmux.conf $HOME/.tmux.conf
 
@@ -98,12 +125,19 @@ mkdir -p $HOME/.tmuxp 2>/dev/null
 link_file $D_TMUX_HOME/landscape.yaml $HOME/.tmuxp/landscape.yaml
 link_file $D_TMUX_HOME/portrait.yaml $HOME/.tmuxp/portrait.yaml
 
+# mycli
+D_MYCLI_HOME=$DOTFILES_ROOT/mycli
+link_file $D_MYCLI_HOME/myclirc $HOME/.myclirc
+
+# zsh
 ZSH_HOME=$HOME/.config/zsh
 D_ZSH_HOME=$DOTFILES_ROOT/zsh
 mkdir -p $ZSH_HOME 2>/dev/null 
 
 export ZPLUG_HOME=$ZSH_HOME/zplug
-git clone https://github.com/zplug/zplug $ZPLUG_HOME
+if [[ ! -d $ZPLUG_HOME ]]; then
+    git clone https://github.com/zplug/zplug $ZPLUG_HOME
+fi 
 
 link_file $D_ZSH_HOME/zshrc $HOME/.zshrc
 link_file $D_ZSH_HOME/lplug $ZSH_HOME/lplug
